9337db6fc375245e7a353279155d4fa6d3edbe57
Biruk Kebede <biruk.key@gmail.com>
2021-05-23 16:11:51 +0100

    Set cookies policy to sameSite=none

--- server/server.js
+++ server/server.js
@@ -39,6 +39,7 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Origin',
     'Origin, X-Requested-With, Content-Type, Accept'
   );
+  res.cookie({ sameSite: 'none', secure: true });
   next();
 });
 
