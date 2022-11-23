4229c33f33e96cc909f6a501cb6da0327fe4e98f
Biruk Kebede <biruk.key@gmail.com>
2022-11-23 04:52:53 +0000

    removed static serve

--- server/server.js
+++ server/server.js
@@ -19,14 +19,7 @@ import deleteVideo from './router/deleteVideo.js';
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
-//app.use(express.static(path.resolve(__dirname, '../client/build')));
-//app.use(express.static(path.join(__dirname, 'build')));
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('../client/build'));
-	app.get('*', (req, res) =>
-		res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
-	);
-}
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
